4105f4bb63396f59b25bb315859a118c94af7d9d
Thomas-B04 <Thomas_Bunyan@outlook.com>
2025-09-11 18:42:39 +0100

    Reorganize greeting messages in Hello_World.js for better structure

--- Hello_World.js
+++ Hello_World.js
@@ -1,5 +1,11 @@
 var greeting = "Hello, World!";
-var goodbye = "Goodbye, World!";
-
 console.log(greeting);
+
+var greeting2 = "Welcome to node.js programming!";
+console.log(greeting2);
+
+var sentence1 = "Learn how to debug code with the debugger.";
+console.log(sentence1);
+
+var goodbye = "Goodbye, World!";
 console.log(goodbye);
